438a4fb475bdc37808b63105bfeed2e454ba7b7d
Angel D. Munoz <scythes.lol@gmail.com>
2020-10-20 21:21:41 -0600

    update deployscript

--- deploy.ps1
+++ deploy.ps1
@@ -1,13 +1,7 @@
-Param (
-    [Parameter(Mandatory=$true)][string]$lambda
-)
 $root = $pwd
-switch ($lambda) {
-    "Mandabot" { set-location "$root/src/Mandabot" }
-    "MandabotGetNotes" { set-location "$root/src/Mandabot.GetNotes" }
-}
+set-location "$root/src/Mandabot"
 dotnet lambda deploy-function $lambda
 if (-Not $?) {
-    write-output "The deploy failed"
+    write-output "The deploy failed..."
 }
 set-location $root
